20dd7a99bca9980de44b2f59cb75a9dcdb01a295
Quarix <144305875+uarix@users.noreply.github.com>
2025-01-06 09:58:17 +0800

    :package: update dev dependency

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,18 +1,23 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const ChromeExtensionReloader = require('webpack-chrome-extension-reloader')
+const { merge } = require('webpack-merge');  
+const common = require('./webpack.common.js');  
+const ExtReloader = require('webpack-ext-reloader');  
 
-module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    mode: 'development',
-    watch: true,
-    watchOptions: {
-        ignored: ['node_modules/**']
-    },
-    plugins: [
-        new ChromeExtensionReloader({
-            port: 9090, // Which port use to create the server
-            reloadPage: true, // Force the reload of the page also
-        })
-    ]
-})
+module.exports = merge(common, {  
+    devtool: 'inline-source-map',  
+    mode: 'development',  
+    watch: true,  
+    watchOptions: {  
+        ignored: ['node_modules/**']  
+    },  
+    plugins: [  
+        new ExtReloader({  
+            port: 9090,   
+            reloadPage: true,  
+            entries: {   
+                background: 'background',  
+                popup: 'popup',  
+                content: 'content'  
+            }  
+        })   
+    ]  
+});
