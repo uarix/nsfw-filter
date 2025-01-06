09f3d0c1b5d86f29a68723bbafe2bf66c0397828
Quarix <144305875+uarix@users.noreply.github.com>
2025-01-06 10:33:05 +0800

    :wrench: fix

--- test/jest.e2e.global_setup.js
+++ test/jest.e2e.global_setup.js
@@ -1,35 +1,62 @@
-const path = require('path');
-const fs = require('fs');
-const os = require('os');
-const mkdirp = require('mkdirp');
-const puppeteer = require('puppeteer');
+const path = require('path');  
+const fs = require('fs');  
+const os = require('os');  
+const mkdirp = require('mkdirp');  
+const puppeteer = require('puppeteer');  
 
-const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');  
+const extensionPath = path.join(__dirname, '../dist');  
+const delay = ms => new Promise(res => setTimeout(res, ms));  
 
-const extensionPath = path.join(__dirname, '../dist')
-const delay = ms => new Promise(res => setTimeout(res, ms));
+module.exports = async function () {  
+  try {  
+    const browserOptions = {  
+      headless: process.env.HEADLESS !== 'false',  
+      product: 'chrome',  
+      defaultViewport: null,  
+      args: [  
+        `--disable-extensions-except=${extensionPath}`,  
+        `--load-extension=${extensionPath}`,  
+        '--disable-dev-shm-usage',  
+        '--no-sandbox',  
+        '--disable-setuid-sandbox'  
+      ]  
+    };  
 
-module.exports = async function () {
-    
-  const browser = await puppeteer.launch({
-    headless: false,
-    product: "chrome",
-    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-    defaultViewport: null,
-    args: [
-    `--disable-extensions-except=${extensionPath}`,
-    `--load-extension=${extensionPath}`,
-    `--disable-dev-shm-usage`,
-    `--no-sandbox`,
-    `--disable-setuid-sandbox`
-    ]});
-  // store the browser instance so we can teardown it later
-  // this global is only available in the teardown but not in TestEnvironments
-  // wait one minute for extension to load
-  global.__BROWSER_GLOBAL__ = browser;
+    if (process.env.CI) {  
+      if (process.env.CHROME_PATH) {  
+        browserOptions.executablePath = process.env.CHROME_PATH;  
+      } else if (process.env.PUPPETEER_EXEC_PATH) {  
+        browserOptions.executablePath = process.env.PUPPETEER_EXEC_PATH;  
+      }  
+    }  
 
-  // use the file system to expose the wsEndpoint for TestEnvironments
-  mkdirp.sync(DIR);
-  fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
-  await delay(5000);
+    console.log('Launching browser with options:', {  
+      ...browserOptions,  
+      executablePath: browserOptions.executablePath ? 'Custom path set' : 'Default'  
+    });  
+
+    const browser = await puppeteer.launch(browserOptions);  
+
+    global.__BROWSER_GLOBAL__ = browser;  
+
+    mkdirp.sync(DIR);  
+    fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());  
+
+    console.log('Waiting for extension to load...');  
+    await delay(5000);  
+    console.log('Extension loaded, continuing with tests...');  
+
+  } catch (error) {  
+    console.error('Error during browser setup:', error);  
+
+    console.log('Environment variables:', {  
+      CI: process.env.CI,  
+      CHROME_PATH: process.env.CHROME_PATH,  
+      PUPPETEER_EXEC_PATH: process.env.PUPPETEER_EXEC_PATH,  
+      HEADLESS: process.env.HEADLESS  
+    });  
+
+    throw error;  
+  }  
 };
